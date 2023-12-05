f77c127d669976e36a01c3f7880506654d114c53
Akifer23 <134030941+Akifer23@users.noreply.github.com>
2023-12-05 16:40:42 -0600

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,6 @@
 function submitForm() {
   alert("Formulario enviado correctamente");
-  // Aquí puedes agregar la lógica para enviar el formulario a tu servidor, si es necesario.
+  
 }
 
 document.addEventListener("DOMContentLoaded", function () {
