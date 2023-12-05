97edc5195c58e07d766b9f3b1b3339784317490f
Akifer23 <134030941+Akifer23@users.noreply.github.com>
2023-12-05 13:29:25 -0600

    Delete js directory

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-function submitForm() {
-  alert("Formulario enviado correctamente");
-  // Aquí puedes agregar la lógica para enviar el formulario a tu servidor, si es necesario.
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("container").style.opacity = 1;
-});
